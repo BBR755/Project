afca8045b9b5cac6bb10326b6c34a95ced3f32c5
BBR <serezha755@mail.ru>
2020-01-12 16:02:09 +0300

    Главный корабль двигается

--- Space_ship.py
+++ Space_ship.py
@@ -1,13 +1,20 @@
 import pygame
 import os
 import sys
+
 pygame.init()
-size = width, height = 1024, 768
+size = width, height = 1280, 600
 screen = pygame.display.set_mode(size)
 screen.fill((50, 80, 255))
 clock = pygame.time.Clock()
-FPS = 50
-
+FPS = 60
+run_x = False
+run_y = False
+x_pos = width//2
+y_pos = height//2
+v_x = 0
+v_y = 0
+speed = 150
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -57,45 +64,60 @@ def start_screen():
         clock.tick(FPS)
 
 
-class ship(pygame.sprite.Sprite):
-    image = load_image("ship.png", -1)
+class Ship(pygame.sprite.Sprite):
+    image = load_image("ship_3.png", -1)
 
-
-    def __init__(self, group):
-        super().__init__(group)
-        print(11111)
-        self.image = ship.image
+    def __init__(self, player_group):
+        super().__init__(player_group)
+        self.image = Ship.image
         self.rect = self.image.get_rect()
         self.rect.x = width//2
         self.rect.y = height//2
 
-    def update(self, *args):
-        print(4545)
-        if args and key[pygame.K_DOWN] and self.rect.y + 10 <= height - 280:
-            self.rect.y += 10
-        if args and key[pygame.K_UP] and self.rect.y - 10 >= 0:
-            self.rect.y -= 10
-        if args and key[pygame.K_LEFT] and self.rect.x - 10 >= 0:
-            self.rect.x -= 10
-        if args and key[pygame.K_RIGHT] and self.rect.x + 10 <= width - 280:
-            self.rect.x += 10
+    def update(self, x_1, y_1):
+        self.rect = self.image.get_rect().move(x_1, y_1)
 
 
 all_sprites = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+ship = Ship(player_group)
 start_screen()
+l = 0
 running = True
 while running:
     for event in pygame.event.get():
-        key = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            all_sprites.update(event)
 
+    key = pygame.key.get_pressed()
     screen.fill(pygame.Color(0, 0, 0))
-    all_sprites.draw(screen)
-    all_sprites.update()
+    if key[pygame.K_LEFT]:
+        v_x = -speed
+    if key[pygame.K_RIGHT]:
+        v_x = speed
+    if key[pygame.K_DOWN]:
+        v_y = speed
+    if key[pygame.K_UP]:
+        v_y = -speed
+    else:
+        if not key[pygame.K_LEFT]:
+            if v_x == -speed:
+                v_x = 0
+        if not key[pygame.K_RIGHT]:
+            if v_x == speed:
+                v_x = 0
+        if not key[pygame.K_UP]:
+            if v_x == -speed:
+                v_y = 0
+        if not key[pygame.K_DOWN]:
+            if v_x == speed:
+                v_y = 0
+    x_pos += v_x / FPS
+    y_pos += v_y / FPS
+    player_group.update(x_pos, y_pos)
+    player_group.draw(screen)
     pygame.display.flip()
+    clock.tick(FPS)
     #clock.tick(1000)
 
 terminate()
